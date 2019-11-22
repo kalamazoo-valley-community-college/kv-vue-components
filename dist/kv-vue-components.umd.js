956609b149f05342bf65ce1c919888383ac16873
adanoski <adanoski@adanoski-imac.kvcc.dom>
2019-11-22 10:09:30 -0500

    Changed a thing to make sure it's working

--- dist/kv-vue-components.umd.js
+++ dist/kv-vue-components.umd.js
@@ -162,12 +162,12 @@ if (typeof window !== 'undefined') {
 // Indicate to webpack that this file can be concatenated
 /* harmony default export */ var setPublicPath = (null);
 
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"591d187e-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/kvButton.vue?vue&type=template&id=26c8b6be&
-var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('button',{staticClass:"leading-none\n                rounded\n                cursor-pointer\n                text-blue-800 hover:text-white focus:text-white hover:text-shadow focus:text-shadow\n                bg-gray-200 hover:bg-blue-700 focus:bg-blue-700\n                border border-gray-500 hover:border-blue-800 focus:border-blue-800\n                focus:shadow-outline\n                font-medium",class:_vm.classes,attrs:{"type":_vm.type}},[_c('span',[_vm._v(_vm._s(_vm.text))])])}
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"740c6b12-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/kvButton.vue?vue&type=template&id=2b4aa357&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('button',{staticClass:"leading-none\n                rounded\n                cursor-pointer\n                text-blue-800 hover:text-white focus:text-white hover:text-shadow focus:text-shadow\n                bg-red-200 hover:bg-blue-700 focus:bg-blue-700\n                border border-gray-500 hover:border-blue-800 focus:border-blue-800\n                focus:shadow-outline\n                font-medium",class:_vm.classes,attrs:{"type":_vm.type}},[_c('span',[_vm._v(_vm._s(_vm.text))])])}
 var staticRenderFns = []
 
 
-// CONCATENATED MODULE: ./src/components/kvButton.vue?vue&type=template&id=26c8b6be&
+// CONCATENATED MODULE: ./src/components/kvButton.vue?vue&type=template&id=2b4aa357&
 
 // CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js??ref--12-0!./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/kvButton.vue?vue&type=script&lang=js&
 //
@@ -333,7 +333,7 @@ var component = normalizeComponent(
 )
 
 /* harmony default export */ var kvButton = (component.exports);
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"591d187e-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/kvLabeledInput.vue?vue&type=template&id=17be9a3a&
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"740c6b12-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/kvLabeledInput.vue?vue&type=template&id=17be9a3a&
 var kvLabeledInputvue_type_template_id_17be9a3a_render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"relative w-48"},[_c('label',{staticClass:"text-xs font-thin mb-1 absolute top-0 left-0 ml-3 mt-2 pointer-events-none",attrs:{"for":_vm.id}},[_vm._v(_vm._s(_vm.label))]),_c('input',{staticClass:"px-3 pb-2 pt-6\n                mb-4\n                border border-gray-500 focus:border-blue-800\n                rounded\n                transition\n                outline-none\n                focus:shadow-outline\n                w-full",attrs:{"type":_vm.type,"id":_vm.id,"name":_vm.id,"required":_vm.required}})])}
 var kvLabeledInputvue_type_template_id_17be9a3a_staticRenderFns = []
 

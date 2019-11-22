5a07e8c2e22b657faf5538845166a232c81dfa07
Nick Frost <nickfrost.us@gmail.com>
2019-11-22 10:43:44 -0500

    class cleanup for labeled input

--- src/components/kvLabeledInput.vue
+++ src/components/kvLabeledInput.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="relative w-48">
         <label :for="id"
-               class="text-xs font-thin mb-1 absolute top-0 left-0 ml-3 mt-2 pointer-events-none">{{ label }}</label>
+               class="absolute top-0 left-0
+               mb-1 ml-3 mt-2
+               text-xs font-thin
+               pointer-events-none">{{ label }}</label>
         <input :type="type" :id="id" :name="id" class="px-3 pb-2 pt-6
-                    mb-4
                     border border-gray-500 focus:border-blue-800
                     rounded
-                    transition
                     outline-none
-                    focus:shadow-outline
-                    w-full"
+                    focus:shadow-outline"
                v-model="input"
                @keyup="updateParentModel"
                :required="required">

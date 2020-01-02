d051bde41bbbe3ad428cbfdd73fcc74d670f3316
Nick Frost <nickfrost.us@gmail.com>
2020-01-02 15:09:42 -0500

    button border interactive updates

--- dist/kv-vue-components.common.js
+++ dist/kv-vue-components.common.js
@@ -1236,12 +1236,12 @@ if (typeof window !== 'undefined') {
 // Indicate to webpack that this file can be concatenated
 /* harmony default export */ var setPublicPath = (null);
 
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"47986298-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/kvButton.vue?vue&type=template&id=8688f230&
-var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('button',{staticClass:"leading-none\n                rounded\n                cursor-pointer\n                text-blue-800 hover:text-white focus:text-white\n                border border-gray-500 hover:border-blue-500 focus:border-blue-500\n                bg-white hover:bg-blue-700 focus:bg-blue-700\n                shadow\n                hover:shadow-outline focus:shadow-outline\n                font-medium",class:_vm.buttonSize,attrs:{"type":_vm.type}},[_c('span',[_vm._v(_vm._s(_vm.text))])])}
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"47986298-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/kvButton.vue?vue&type=template&id=408e2a62&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('button',{staticClass:"leading-none\n                rounded\n                cursor-pointer\n                text-blue-800 hover:text-white focus:text-white\n                border border-gray-500 hover:border-blue-800 focus:border-blue-800\n                bg-white hover:bg-blue-700 focus:bg-blue-700\n                shadow\n                hover:shadow-outline focus:shadow-outline\n                font-medium",class:_vm.buttonSize,attrs:{"type":_vm.type}},[_c('span',[_vm._v(_vm._s(_vm.text))])])}
 var staticRenderFns = []
 
 
-// CONCATENATED MODULE: ./src/components/kvButton.vue?vue&type=template&id=8688f230&
+// CONCATENATED MODULE: ./src/components/kvButton.vue?vue&type=template&id=408e2a62&
 
 // CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js??ref--12-0!./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/kvButton.vue?vue&type=script&lang=js&
 //

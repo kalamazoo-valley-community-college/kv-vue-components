0e4d418581e130e9dd073144d240e9ee4de91f1f
Nick Frost <nickfrost.us@gmail.com>
2020-02-03 08:48:25 -0500

    computed local model for labeled input

--- src/components/kvLabeledInput.vue
+++ src/components/kvLabeledInput.vue
@@ -11,14 +11,14 @@
                :class="classes"
                :style="styling"
                :list="list_id"
-               v-model="input"
+               v-model="local_model"
                :required="required"
                @keyup="updateParentModel">
         <select v-if="type === 'select'"
                 :id="id" :name="id"
                 :class="classes"
                 :style="styling"
-                v-model="input"
+                v-model="local_model"
                 @change="updateParentModel">
             <!-- Allow a slot for options directly if this is a select input. -->
             <slot/>
@@ -44,6 +44,11 @@
             model: null,
             required: Boolean,
             list_id: String
+        },
+        computed: {
+            local_model() {
+                return this.model;
+            }
         },
         data() {
             return {
